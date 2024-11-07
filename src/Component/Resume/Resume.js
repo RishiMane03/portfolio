efded4954ec10176feaed6653becf2f09175f475
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2024-11-07 09:47:56 +0530

    Update Resume.js

--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -52,7 +52,7 @@ const Resume = () => {
           </div>
           <div className="resumeImg_wrapper_2">
             <img className="resumeImg_2" src={resume_img} alt="resume" />
-            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1hZlGwr1wFsdS2XBLg86muzZ75BYlIw1e/view?usp=sharing">
+            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1MNnLjTM8ZD1c6t0vXjEj6xJ59lkIY5tq/view?usp=sharing">
               <div className="open-button">
                 Open
               </div>
